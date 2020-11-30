7571f8baf02a8ff3b3da865ae1bd0de84f665d84
Hayaoru <73983834+Hayaoru@users.noreply.github.com>
2020-11-30 17:55:54 +0900

    新規投稿と編集機能が同時にできないエラーの解消

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -25,9 +25,7 @@ class PrototypesController < ApplicationController
   end
 
   def create
-  
     @prototype = Prototype.create(prototype_params)
- 
     if @prototype.save
       redirect_to root_path
     else 
